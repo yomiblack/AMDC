ae709d008692a8afc14a2f0b10d4dee987712e49
Yomiblack <yomiblacck@gmail.com>
2025-03-26 15:07:34 +0100

    vercel analytics added

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
